ff63aea668f264cac8f5085a3d93b07d1051cf59
Leo Fidjeland <leo.fidjeland@gmail.com>
2016-04-21 16:58:13 +0200

    Dockerized

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# This indicates what base image you are using and who maintains the Dockerfile.
+FROM hypriot/rpi-python
+MAINTAINER Leo Fidjeland (hello@leo.wtf)
+
+RUN apt-get install python-rpi.gpio
+
+# Upgrade pip
+RUN pip install --upgrade pip
+
+# We add requirements first so that we don't have to rebuild everytime we change the source
+ADD requirements.txt /opt
+RUN pip install -r /opt/requirements.txt
+
+# Add the source
+ADD src /opt/eem
+
+# We add the default config parameters to display the available options
+ENV MQTT localhost
+ENV MQTT_USER user
+ENV MQTT_PASS password
+ENV MQTT_PREFIX MainMeter
+ENV MQTT_CLIENT EnergyLogger
+ENV EEM_GPIO_PIN 23
+
+
+# Go go go!
+CMD ["python","-u","/opt/eem/EnergyMeterPulsReaderMQTT.py"]
--- src/EnergyMeterPulsReaderMQTT.py
+++ src/EnergyMeterPulsReaderMQTT.py
@@ -9,9 +9,17 @@ import json
 import urllib2
 import base64
 from math import fabs
-import mosquitto
+import paho.mqtt.client as mqtt
 
 
+config = {
+    'mqtt_host': os.environ.get('MQTT','localhost'),
+    'mqtt_user': os.environ.get('MQTT_USER','driver'),
+    'mqtt_pass': os.environ.get('MQTT_PASS','1234'),
+    'mqtt_prefix': os.environ.get('MQTT_PREFIX','MainMeter'),
+    'mqtt_client': os.environ.get('MQTT_CLIENT','EnergyLogger'),
+    'pin': int(os.environ.get('EEM_GPIO_PIN',23))
+}
 
 #Functions
 #Time
@@ -20,14 +28,14 @@ def CurrentTime():
 
 
 #Class
-class EnergyLogger(mosquitto.Mosquitto):
+class EnergyLogger(mqtt.Client):
 
-	def __init__(self,pin=23,user = "driver", password="1234",server = "localhost", prefix = "MainMeter",client = "EnergyLogger"):
+	def __init__(self,pin=config['mqtt_host'],user = config['mqtt_user'], password=config['mqtt_pass'],server = config['mqtt_host'], prefix = config['mqtt_prefix'],client = config['mqtt_client']):
 
 		self.Factor = 0.01 # kWh per pulse
 		self.Threshhold = 10.0
 		self.SentThreshhold = None
-	
+
 		self.LastTime = 0.0
 		self.Counter = 0.0
 		self.LastPeriod = 0.0
@@ -36,21 +44,21 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		self.error_threshhold = 70000
 
-		self.pulse_lenght = 0.080 
+		self.pulse_lenght = 0.080
 		self.pulse_lenght_max_dev = 0.0005
 
 		self.pin = pin
 
-	
+
 		self.prefix = prefix
 
 		#Init and connect to MQTT server
-		mosquitto.Mosquitto.__init__(self,client)
+		mqtt.Client.__init__(self,client)
 		self.will_set( topic = "system/" + self.prefix, payload="Offline", qos=1, retain=True)
 
 		if user != None:
     			self.username_pw_set(user,password)
-    			
+
     		self.on_connect = self.mqtt_on_connect
     		self.on_message = self.mqtt_on_message
 
@@ -69,7 +77,7 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		return
 
-	#Posting data to couchDB  
+	#Posting data to couchDB
 	def Update(self,sub_topic,value,timestamp = time()):
 
 		topic = self.prefix+"/"+sub_topic
@@ -78,15 +86,15 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		#print "New event: " + topic
         	self.publish(topic,msg,1)
-		
+
 		return
 
 	def SendMeterEvent(self,timestamp,power,energy,threshhold):
 		#self.Update("power",power,timestamp)
 		#self.Update("counter",counter,timestamp)
-		
+
 		topic = self.prefix+"/meterevent"
-		
+
 		msg = json.dumps({"time":timestamp,"power":power,"energy":energy})
 
 		self.publish(topic,msg,1)
@@ -104,13 +112,13 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 	def my_callback2(self,channel):
 
-    		TimeStamp = time()    
-		#print CurrentTime()		
+    		TimeStamp = time()
+		#print CurrentTime()
 
 		#Detect high or low
 		if not GPIO.input(channel):
-			self.Falling = TimeStamp		
-			print CurrentTime()	
+			self.Falling = TimeStamp
+			print CurrentTime()
 			print "Falling edge on " + str(channel)
 			return
 		else:
@@ -118,7 +126,7 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		#Check pulse lenght.
 		PulseLenght = TimeStamp - self.Falling
-		PulseDeviation = fabs(PulseLenght - self.pulse_lenght)		
+		PulseDeviation = fabs(PulseLenght - self.pulse_lenght)
 
 		print "Pulse lengt on port %i was %.2fms\na deviation of %.3fms from expected %.2fms" %(self.pin,PulseLenght*1000,PulseDeviation *1000,self.pulse_lenght *1000)
 
@@ -126,7 +134,7 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		if PulseDeviation >  self.pulse_lenght_max_dev:
 			print "Pulselenght error"
-			return 
+			return
 
 
 		if self.LastTime == 0:
@@ -137,18 +145,18 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		#Debounce function
 		#if Period < 1.0:
-		#	return 
+		#	return
 
 		self.Counter += 1
 
 		self.LastTime = TimeStamp
 		self.LastPeriod = Period
 
-    		#Calculate values. 
+    		#Calculate values.
     		Energy = self.Counter * self.Factor
     		Power = self.Factor / (Period / 3600000.0) # The energy divided on the time in hours.
     		Delta = fabs(Power - self.LastPower)
-		
+
 		print "Period is: %.2f s \nPower is: %.2f W\nEnergy: %.2f kWh\nChange: %.2f " % (Period,Power,Energy,Delta)
 		print " "
 
@@ -166,26 +174,26 @@ class EnergyLogger(mosquitto.Mosquitto):
 
 		print "Period is: %.2f s \nPower is: %.2f W\nEnergy: %.2f kWh\nChange: %.2f " % (Period,Power,Energy,Delta)
 
-		if Delta > self.Threshhold: 
+		if Delta > self.Threshhold:
 			print "Updating..."
-			
+
 			self.SendMeterEvent(str(TimeStamp),str(Power),str(Energy),str(self.Threshhold))
 			self.SendIOEvent(str(TimeStamp),str(Period),str(self.Counter),str(PulseLenght))
 		return
-	
+
 	def mqtt_on_connect(self, selfX,mosq, result):
     		print "MQTT connected!"
     		self.subscribe(self.prefix + "/#", 0)
-    
+
   	def mqtt_on_message(self, selfX,mosq, msg):
     		print("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
-    	
+
     		return
 
 
 if __name__ == "__main__":
 
-	
+
 	#raw_input("Press Enter when ready\n>")
 
 	Logger = EnergyLogger()
@@ -202,4 +210,3 @@ if __name__ == "__main__":
 	except KeyboardInterrupt:
     		GPIO.cleanup()       # clean up GPIO on CTRL+C exit
 	GPIO.cleanup()           # clean up GPIO on normal exit
-
