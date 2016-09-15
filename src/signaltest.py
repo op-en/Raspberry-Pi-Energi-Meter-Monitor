85363d43a2d266241f0849541393aac1af8a15c3
Anton Gustafsson <anton.gustafsson@tii.se>
2016-09-15 11:10:23 +0200

    BF

--- src/signaltest.py
+++ src/signaltest.py
@@ -4,10 +4,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 oldinput = False
 count=0
+oldtime = 0
 
 while (1):
     input = GPIO.input(23)
     if input != oldinput:
-        print(count, input, time.time())
+        now = time.time()
+        delta = now - oldtime
+        print(count, input, now, delta)
+        oldtime = now
         oldinput = input
-        count++
+        count =+ 1
