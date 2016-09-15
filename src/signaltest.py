1b85aa881a1972d62449cc9c0fbbf8ed0a88d7eb
Anton Gustafsson <anton.gustafsson@tii.se>
2016-09-15 11:09:40 +0200

    Improved test script

--- src/signaltest.py
+++ src/signaltest.py
@@ -3,9 +3,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 oldinput = False
+count=0
 
 while (1):
     input = GPIO.input(23)
     if input != oldinput:
-        print(input, time.time())
+        print(count, input, time.time())
         oldinput = input
+        count++
