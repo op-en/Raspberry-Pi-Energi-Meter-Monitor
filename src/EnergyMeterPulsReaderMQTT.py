c8765822f0cc38c9724a9babd6746e29d08673f7
Anton Gustafsson <anton.gustafsson@tii.se>
2016-09-15 17:04:09 +0200

    New version

--- src/EnergyMeterPulsReaderMQTT.py
+++ src/EnergyMeterPulsReaderMQTT.py
@@ -18,7 +18,8 @@ config = {
     'mqtt_pass': os.environ.get('MQTT_PASS',None),
     'mqtt_prefix': os.environ.get('MQTT_PREFIX','rpi-emm'),
     'mqtt_client': os.environ.get('MQTT_CLIENT','EnergyLogger'),
-    'pin': int(os.environ.get('EMM_GPIO_PIN',23))
+    'pin': int(os.environ.get('EMM_GPIO_PIN',23)),
+    'factor': int(os.environ.get('EMM_PULSE_FACTOR',0.01))
 }
 
 #Functions
@@ -33,22 +34,26 @@ class EnergyLogger(mqtt.Client):
 	def __init__(self,pin=config['mqtt_host'],user = config['mqtt_user'], password=config['mqtt_pass'],server = config['mqtt_host'], prefix = config['mqtt_prefix'],client = config['mqtt_client']):
 
 		self.Factor = 0.01 # kWh per pulse
-		self.Threshhold = 10.0
+		self.Threshhold = 10.0 # Dont update if power didnt change more than this amount.
 		self.SentThreshhold = None
 
 		self.LastTime = 0.0
-		self.Counter = 0.0
+		self.EnergyCounter = 0
+        self.PulseCounter
 		self.LastPeriod = 0.0
 		self.Falling = 0.0
 		self.LastPower = 0.0
 
-		self.error_threshhold = 70000
+		self.error_threshhold = 100000
 
 		self.pulse_lenght = 0.080
-		self.pulse_lenght_max_dev = 0.0005
+		self.pulse_lenght_max_dev = 0.005
+        self.pulse_lenght_buf = []
+        self.auto_pulselenght = True
 
 		self.pin = pin
 
+        self.debug = True
 
 		self.prefix = prefix
 
@@ -71,11 +76,111 @@ class EnergyLogger(mqtt.Client):
 		GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 		# when a falling or rising edge is detected on port self.pin, call callback2
-		GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.my_callback2, bouncetime=0)
+		#GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.my_callback2, bouncetime=0)
 
 		self.loop_start()
 
 		return
+
+    def StartDetection():
+        count=0
+        oldtime = 0
+        edges = []
+
+        timeout = 10000
+
+        while True:
+            res = GPIO.wait_for_edge(23,GPIO.BOTH,timeout=timeout)
+
+            now = time.time()
+
+            if res != None:
+                edges.append(now)
+                timeout = self.pulse_lenght
+                continue
+
+            if len(edges) == 0:
+                timeout = 10000
+                continue
+
+            #TimeStamp
+            timestamp = edges[0]
+
+            #Calculate pulse length and increase count
+            pulselenght = edges[-1] - edges[0]
+
+            #Calculate period
+            period = edges[-1] - oldtime
+            oldtime = edges[-1]
+
+            #Bounces
+            bounces = len(edges)
+
+            #Clear databuffer.
+            edges=[]
+
+            self.PulseCounter += 1
+
+            if self.auto_pulselenght:
+                self.pulse_lenght_buf.append(pulselenght)
+                self.pulse_lenght_buf=self.pulse_lenght_buf[-100:]
+                self.pulse_lenght = sum(self.pulse_lenght_buf)/len(self.pulse_lenght_buf)
+
+            #Check pulse lenght.
+    		PulseDeviation = fabs(pulselenght - self.pulse_lenght)
+
+            self.SendIOEvent(str("%.3f" % timestamp),"%.2f" % period,str(self.PulseCounter),"%.3f" % pulselenght,str(bounces),"%.3f" % PulseDeviation)
+
+            if self.debug:
+                print("%.2f Pin: %i \tCount: %i \tPulse lenght: %.3f Bounces: %i \tPeriod: %.3f \tPulseDeviation: %.3f" % (timestamp, self.pin,count, pulselenght, bounces ,period,PulseDeviation)
+
+            self.CountEnergy(timestamp,pulselenght,bounces)
+
+    def CountEnergy(self,TimeStamp,PulseLenght,Bounces )
+
+
+    	if self.LastTime == 0:
+    		self.LastTime = TimeStamp
+    		return
+
+        Period = TimeStamp - self.LastTime
+
+
+            #
+    	if PulseDeviation >  self.pulse_lenght_max_dev:
+			print("%.3f Pulselenght error" % TimeStamp)
+            return
+
+
+    	self.EnergyCounter += 1
+
+    	self.LastTime = TimeStamp
+    	self.LastPeriod = Period
+
+        #Calculate values.
+        Energy = self.Counter * self.Factor
+        Power = self.Factor / (Period / 3600000.0) # The energy divided on the time in hours.
+        Delta = fabs(Power - self.LastPower)
+
+
+    	if Delta > self.error_threshhold:
+    		print "%.3f Error: The power value exceeds the error threshhold of %.0f W " % (timestamp, self.error_threshhold)
+    		print " "
+    		return
+
+
+    	#Store for future reference
+    	self.LastPower = Power
+    	self.LastEnergy = Energy
+    	self.LastDelta = Delta
+
+        if self.debug:
+    	     print "Period is: %.2f s \nPower is: %.2f W\nEnergy: %.2f kWh\nChange: %.2f " % (Period,Power,Energy,Delta)
+
+    	if Delta > self.Threshhold:
+    		 self.SendMeterEvent(str(TimeStamp),str(Power),str(Energy),str(self.Threshhold))
+
+        return
 
 	#Posting data to couchDB
 	def Update(self,sub_topic,value,timestamp = time()):
@@ -104,9 +209,9 @@ class EnergyLogger(mqtt.Client):
 
 		return
 
-	def SendIOEvent(self,timestamp,Period,Counter,PulseLenght):
+	def SendIOEvent(self,timestamp,Period,Counter,PulseLenght,Bounces,PulseDeviation):
 		topic = self.prefix+"/ioevent"
-		msg = json.dumps({"time":timestamp,"counter":Counter,"period":Period,"pulselenght":PulseLenght})
+		msg = json.dumps({"time":timestamp,"counter":Counter,"period":Period,"pulselenght":PulseLenght,"bounces":Bounces,"pulsedeviation":PulseDeviation})
 		self.publish(topic,msg,1)
 		return
 
