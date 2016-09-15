4c9fb279eecf95b414bc677f44ae7050e55ca108
Anton Gustafsson <anton.gustafsson@tii.se>
2016-09-15 12:14:27 +0200

    bf

--- src/signaltest2.py
+++ src/signaltest2.py
@@ -8,7 +8,7 @@ count=0
 oldtime = 0
 edges = []
 
-timeout = None
+timeout = 0
 
 while True:
     res = GPIO.wait_for_edge(23,GPIO.BOTH,timeout=timeout)
@@ -21,7 +21,7 @@ while True:
         continue
 
     if len(edges) == 0:
-        timeout = None
+        timeout = 0
         continue
 
     delta = edges[-1] - edges[0]
